367f565c72ff31ff4fc879064dae5b65b9bbb9e9
Ronja <ronja.lipsonen@tuni.fi>
2024-01-25 14:42:41 +0200

    Coded 2.9.

--- Part2/puhelinluettelo/src/App.jsx
+++ Part2/puhelinluettelo/src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
+import {useEffect, useState } from 'react'
 import Phonebook from './components/Phonebook'
 
 const App = (props) => {
-  const [persons, setPersons] = useState(props.persons) 
+  const [persons, setPersons] = useState(props.persons)
+  const [filteredPersons, setFilteredPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [subString, setSubString] = useState('')
+
+  useEffect(() => {
+    setFilteredPersons(persons);
+  }, [persons])
 
   const addPerson = (event) => {
     event.preventDefault()
     if (persons.some(person => person.name === newName)) {
-      window.alert(`${newName} is already added to phonebook!`);
-      return;
+      window.alert(`${newName} is already added to phonebook!`)
+      return
     }
     const personObject = {
       name: newName,
@@ -23,18 +29,32 @@ const App = (props) => {
   }
 
   const handlePersonChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumber = (event) => {
-    console.log(event.target.value)
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    const searchString = event.target.value;
+    setSubString(searchString);
+
+    const filtered = persons.filter((person) =>
+      person.name.toLowerCase().includes(searchString.toLowerCase()))
+    setFilteredPersons(filtered);
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with <input value={subString}
+          onChange={handleFilterChange}/>
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} 
@@ -49,7 +69,7 @@ const App = (props) => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons?.map(person =>
+      {filteredPersons?.map(person =>
          <Phonebook key={person.id} person={person} /> )}
     </div>
   )
--- Part2/puhelinluettelo/src/main.jsx
+++ Part2/puhelinluettelo/src/main.jsx
@@ -7,8 +7,22 @@ const persons = [
         name: 'Arto Hellas',
         id: 1,
         number: '040-123456'
+    },
+    {
+        name: 'Ada Lovelace',
+        id: 2,
+        number: '39-44-5323523'
+    },
+    {
+        name: 'Dan Abramov',
+        id: 3,
+        number: '12-43-234345'
+    },
+    {
+        name: 'Mary Poppendieck',
+        id: 4,
+        number: '39-23-6423122'
     }
-    
 ]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
