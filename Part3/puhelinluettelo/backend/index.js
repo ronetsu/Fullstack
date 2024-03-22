eed6745b5da7522668f3f8c1d713f2428a031da7
Ronja Lipsonen <ronja.lipsonen@tuni.fi>
2024-03-22 15:07:26 +0200

    Error handling middleware added.

--- Part3/puhelinluettelo/backend/index.js
+++ Part3/puhelinluettelo/backend/index.js
@@ -3,7 +3,6 @@ const morgan = require("morgan");
 const app = express();
 
 const path = require("path");
-console.log("Resolved path to .env file:", path.resolve(__dirname, "../.env"));
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 console.log("MongoDB URI:", process.env.MONGODB_URI);
 const Person = require("./models/person");
@@ -16,8 +15,22 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if(error.name === "CastError") {
+    return response.status(400).send({error: "malformatted id"});
+  }
+
+  next(error);
+}
+
 app.use(cors());
 app.use(express.json());
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get("/api/persons", (req, res) => {
   Person.find({}).then((persons) => {
@@ -25,26 +38,16 @@ app.get("/api/persons", (req, res) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+  .then((person) => {
+    if (person) {
+      response.json(person);
+    } else {
+      response.status(404).end(); 
+    }
+  }).catch(error => next(error))
   console.log(person.id, typeof person.id, id, typeof id, person.id === id);
-});
-
-app.delete("/api/persons/:id", (request, response) => {
-  Person.findByIdAndDelete(request.params.id)
-    .then((result) => {
-      if (result) {
-        response.status(204).end();
-      } else {
-        response.status(404).json({ error: "Person not found" });
-      }
-    })
-    .catch((error) => {
-      console.error("Error removing person:", error);
-      response.status(500).json({ error: "Internal Server Error" });
-    });
 });
 
 app.post("/api/persons", (request, response) => {
@@ -74,11 +77,22 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+        response.status(204).end();
+    })
+    .catch(error => next(error));
+});
+
 app.get("/info", (request, response) => {
   response.send(
     "<p>Phonebook has info for " + persons.length + " people</p>" + new Date()
   );
 });
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
