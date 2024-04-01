82df1c61c17ab115e02cb40ba7b44b9e1fb62b8d
Ronja <ronja.lipsonen@tuni.fi>
2024-04-01 16:21:47 +0300

    Lint in use.

--- Part4/blogilista/backend/controllers/blogs.js
+++ Part4/blogilista/backend/controllers/blogs.js
@@ -1,41 +1,41 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
+const blogsRouter = require("express").Router()
+const Blog = require("../models/blog")
 
-blogsRouter.get('/', (request, response) => {
-    Blog.find({}).then(blogs => {
-        response.json(blogs)
-        })
+blogsRouter.get("/", (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+})
+
+blogsRouter.get("/:id", (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then(blog => {
+      if (blog) {
+        response.json(blog)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
+})
 
-blogsRouter.get('/:id', (request, response, next) => {
-    Blog.findById(request.params.id)
-        .then(blog => {
-            if (blog) {
-                response.json(blog)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+blogsRouter.post("/", (request, response) => {
+  const body = request.body
+  console.log("body:", body.body)
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0
+  })
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
     })
-
-blogsRouter.post('/', (request, response) => {
-    const body = request.body
-    console.log('body:', body.body)
-
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes || 0
-    })
-
-    blog
-        .save()
-        .then(result => {
-        response.status(201).json(result)
-        })
-    }
+}
 )
 
 module.exports = blogsRouter
--- Part4/blogilista/backend/utils/config.js
+++ Part4/blogilista/backend/utils/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config()
 
 let PORT = 3001
 let MONGODB_URI = "mongodb+srv://ronjalipsonen:Ukkoliini12@cluster0.km2kn07.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
