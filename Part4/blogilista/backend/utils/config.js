d44644b3f63c832709cc663b66ae032bf8b27e1f
Ronja Lipsonen <ronja.lipsonen@tuni.fi>
2024-04-04 13:04:16 +0300

    Commit to hide old commits...

--- Part4/blogilista/backend/utils/config.js
+++ Part4/blogilista/backend/utils/config.js
@@ -2,7 +2,10 @@ const path = require("path")
 require("dotenv").config({ path: path.resolve(__dirname, "../../.env") })
 
 let PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
+
+const MONGODB_URI = process.env.NODE_ENV === 'test' 
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
