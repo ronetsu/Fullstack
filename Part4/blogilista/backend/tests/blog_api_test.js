2a58d61a9b18184a401100f9e58365e24ef25c6f
Ronja Lipsonen <ronja.lipsonen@tuni.fi>
2024-04-04 13:46:06 +0300

    4.8 done but not tested well.

---
+++ Part4/blogilista/backend/tests/blog_api_test.js
@@ -0,0 +1,27 @@
+const { test, after, beforeEach } = require("node:test")
+const assert = require("node:assert")
+const mongoose = require("mongoose")
+const supertest = require("supertest")
+const app = require("../app")
+const api = supertest(app)
+
+const helper = require("./test_helper")
+
+const Blog = require("../models/blog")
+
+beforeEach(async () => {
+  //  await Blog.deleteMany({})
+
+    await Blog.insertMany(helper.initialBlogs)
+})
+
+test.only("blogs are returned as json", async () => {
+    await api
+        .get("/api/blogs")
+        .expect(200)
+        .expect("Content-Type", /application\/json/)
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
